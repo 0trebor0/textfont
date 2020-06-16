99a81066bcd381f2d5279239cada55927c56ef16
0trebor0 <public0projects@outlook.com>
2020-06-16 14:46:07 +0100

    first release

---
+++ example.js
@@ -0,0 +1,10 @@
+const textfont = require('./index');
+// textfont.true();
+// console.red("RED");
+// console.blue("BLUE");
+// console.yellow("YELLOW");
+// console.green("GREEN");
+// console.white("WHITE");
+
+
+//console.log( textfont.fonts.blue, "HELLO" );
---
+++ index.js
@@ -0,0 +1,24 @@
+const colors  = {
+    fonts:{
+        white:"\x1b[37m%s\x1b[0m",
+        red:"\x1b[31m%s\x1b[0m",
+        green:"\x1b[32m%s\x1b[0m",
+        blue:"\x1b[34m%s\x1b[0m",
+        yellow:"\x1b[33m%s\x1b[0m"
+    },
+    background:{
+        white:"\x1b[47m%s\x1b[0m",
+        red:"\x1b[41m%s\x1b[0m",
+        green:"\x1b[42m%s\x1b[0m",
+        blue:"\x1b[44m%s\x1b[0m",
+        yellow:"\x1b[43m%s\x1b[0m"
+    },
+    true:function(){
+        for(let x in colors.fonts ){
+            console[x] = (text)=>{
+                console.log( colors.fonts[x], text );
+            }
+        }
+    }
+}
+module.exports = colors;
